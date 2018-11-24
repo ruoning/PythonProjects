02ed4b35c8a03ed61db34c109d150f05a82e4121
许涛 <xutao@1programmer.com>
2018-11-24 20:21:48 +0800

    change

--- hw1.py
+++ hw1.py
@@ -1,31 +1,31 @@
-PATH = "/Users/ruoningli/Desktop/ruoning/test.txt"
 
-lines = 0
-age = 0
-birth = 0
 
-greatest_age = ""
-greatest_birth = ""
+PATH = "data/hw1.txt"
 
-with open(PATH) as file_doc:
-    for data in file_doc.readlines():
-        if data == "\n":
-            continue
-        lines += 1
-        print(data)
 
-        words = data.split(',')
-        if words[1] > age:
-            age = words[1]
-            greatest_age = words[0]
-
-        dates = words[2].split('-')
-        if dates[2] > birth:
-            birth = dates[2]
+def open_test_file(path=PATH):
+    """打开测试文件"""
+    lines = 0
+    age = 0
+    birth = 0
+    greatest_age = ""
+    greatest_birth = ""
+    with open(path) as file_doc:
+        for data in file_doc.readlines():
+            if data == "\n":
+                continue
+            lines += 1
+            print(data)
+            words = data.split(',')
+            if words[1] > age:
+                age = words[1]
+                greatest_age = words[0]
+            dates = words[2].split('-')
+            if dates[2] > birth:
+                birth = dates[2]
             greatest_birth = words[0]
 
-print("Total number of kids is " + str(lines))
-print("The kid with greatest age is " + greatest_age)
-print("The kid with greatest date of birth is " + greatest_birth)
 
-
+if __name__ == "__main__":
+    print("start do hw1.py")
+    open_test_file()
