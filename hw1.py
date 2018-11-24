42d201b85da703d4af9377408b6fb9cd8b3b4803
许涛 <xutao@1programmer.com>
2018-11-24 20:42:43 +0800

    change

--- hw1.py
+++ hw1.py
@@ -24,6 +24,32 @@ def open_test_file(path=PATH):
     print("max age is %s" % max(age_list))
 
 
+# 用户
+users = [
+    {"id": 0, "name": "l"},
+    {"id": 1, "name": "li"},
+    {"id": 2, "name": "x2"},
+    {"id": 3, "name": "x1"},
+    {"id": 4, "name": "x"},
+    {"id": 5, "name": "xu"},
+    {"id": 6, "name": "xy"},
+
+]
+
+
+# 朋友关系
+friendships = [
+    (0, 1),
+    (0, 2),
+    (1, 2),
+    (1, 3),
+    (2, 3),
+    (3, 4),
+    (4, 5),
+    (5, 6)
+]
+
+
 if __name__ == "__main__":
     print("start do hw1.py")
     open_test_file()
