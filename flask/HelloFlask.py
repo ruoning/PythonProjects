c026321c2f3e5ec3c04f4b7be582e07412ad1564
lihaha <ruoninghappy.li@gmail.com>
2019-02-05 22:51:22 -0800

    Add User Management DB

--- flask/HelloFlask.py
+++ flask/HelloFlask.py
@@ -1,4 +1,6 @@
-from flask import Flask,request
+from flask import Flask, request
+import sqlite3
+
 app = Flask(__name__)
 
 
@@ -12,10 +14,14 @@ def login_page():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        if username == 'admin' and password == '123':
+        conn = sqlite3.connect("UserManagementDB.db")
+        cursor = conn.execute('select * from LoginImformation where UserName=? and Password=?', [username, password])
+        if cursor.fetchone() is not None:
             return 'Successfully login'
         else:
             return 'Please try again'
+        conn.close()
+
     else:
         user_info = request.args.get('user')
         return "Welcome back %s" % user_info
@@ -25,10 +31,34 @@ def login_page():
 def register_page():
     username = request.form.get('username')
     password = request.form.get('password')
+    conn = sqlite3.connect("UserManagementDB.db")
+    conn.execute('insert into LoginImformation (UserName,Password) values (?,?)', [username,password])
+    conn.commit()
+    conn.close()
 
-    # store user information to the database
     return 'Welcome %s, you have successfully registered.' % username
-    
-        
+
+
+@app.route('/unregister', methods=['POST'])
+def unregister_page():
+    username = request.form.get('username')
+    conn = sqlite3.connect("UserManagementDB.db")
+    conn.execute("delete from LoginImformation where UserName=?", [username])
+    conn.commit()
+    conn.close()
+    return 'You have been successfully deleted!'
+
+
+@app.route('/update', methods=['POST'])
+def update_page():
+    password = request.form.get('password')
+    username = request.form.get('username')
+    conn = sqlite3.connect("UserManagementDB.db")
+    conn.execute("update LoginImformation set Password=? where UserName=?",[password,username])
+    conn.commit()
+    conn.close()
+    return 'Successfully update your password'
+
+
 if __name__ == '__main__':
     app.run()
--- flask/client_test.py
+++ flask/client_test.py
@@ -1,9 +1,13 @@
 import requests
 
-user_data = {'username': 'admin', 'password': '123'}
-l = requests.post("http://127.0.0.1:5000/login", data=user_data)
-r = requests.post("http://127.0.0.1:5000/register", data=user_data)
+user_data = {'username': 'admin', 'password': '456'}
+#l = requests.post("http://127.0.0.1:5000/login", data=user_data)
+#r = requests.post("http://127.0.0.1:5000/register", data=user_data)
+#d = requests.post("http://127.0.0.1:5000/unregister", data=user_data)
+u = requests.post("http://127.0.0.1:5000/update", data=user_data)
 
-print('Print '+l.text)
-print('Print '+r.text)
+#print(l.text)
+#print(r.text)
+#print(d.text)
+print(u.text)
 
