378062a06a16188ae1d1aae9e373b78368688108
lihaha <ruoninghappy.li@gmail.com>
2019-02-24 20:01:58 +0800

    update mysql connection

--- flask/HelloFlask.py
+++ flask/HelloFlask.py
@@ -1,7 +1,33 @@
 from flask import Flask, request
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 import sqlite3
 
 app = Flask(__name__)
+
+# 创建对象的基类
+Base = declarative_base()
+
+
+# 定义user对象
+class User(Base):
+
+    # table name
+    __tablename__ = 'userinformation'
+
+    # 表的结构
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+    password = Column(String(20))
+
+
+# init DB connection
+engine = create_engine('mysql+mysqlconnector://root:cluo0626@localhost:3306/MyProject')
+
+
+# create DBSession
+DBSession = sessionmaker(bind=engine)
 
 
 @app.route('/')
@@ -14,8 +40,8 @@ def login_page():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        conn = sqlite3.connect("UserManagementDB.db")
-        cursor = conn.execute('select * from LoginImformation where UserName=? and Password=?', [username, password])
+        #conn = sqlite3.connect("UserManagementDB.db")
+        #cursor = conn.execute('select * from LoginImformation where UserName=? and Password=?', [username, password])
         if cursor.fetchone() is not None:
             return 'Successfully login'
         else:
--- flask/client_test.py
+++ flask/client_test.py
@@ -1,13 +1,13 @@
 import requests
 
 user_data = {'username': 'admin', 'password': '456'}
-#l = requests.post("http://127.0.0.1:5000/login", data=user_data)
+l = requests.post("http://127.0.0.1:5000/login", data=user_data)
 #r = requests.post("http://127.0.0.1:5000/register", data=user_data)
 #d = requests.post("http://127.0.0.1:5000/unregister", data=user_data)
-u = requests.post("http://127.0.0.1:5000/update", data=user_data)
+#u = requests.post("http://127.0.0.1:5000/update", data=user_data)
 
-#print(l.text)
+print(l.text)
 #print(r.text)
 #print(d.text)
-print(u.text)
+#print(u.text)
 
